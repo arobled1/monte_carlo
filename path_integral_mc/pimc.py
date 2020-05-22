ac674da38b6268ce5215a68cab24b624bc16046d
Alan Robledo <ar6138@nyu.edu>
2020-05-22 15:15:51 -0700

    added more comments

--- path_integral_mc/pimc.py
+++ path_integral_mc/pimc.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
+# Transform the whole chain to staging coordinates
 def get_closed_chain_stage_coords(xi, num_beads):
     up_u = np.zeros(num_beads)
     # For the first bead
@@ -13,6 +14,7 @@ def get_closed_chain_stage_coords(xi, num_beads):
     up_u[num_beads-1] = xi[num_beads-1] - ( ( (num_beads-2)*xi[0] ) + xi[0] )/(num_beads - 1)
     return up_u
 
+# Transform the whole chain to primitive coordinates
 def closed_chain_inverse_stage_coords(ui, num_beads):
     up_x = np.zeros(num_beads)
     # For the first bead
@@ -24,6 +26,7 @@ def closed_chain_inverse_stage_coords(ui, num_beads):
         up_x[k] = ui[k] + (k/(k+1))*up_x[k+1] + (1/(k+1))*ui[0]
     return up_x
 
+# Transform a segment of the chain to staging coordinates
 def segment_stage_to_prim(ui, xi, segment_length, rand_bead, num_beads):
     # Define primitive coordinates from staging coordinates.
     for k in range(segment_length, 0, -1):
@@ -31,15 +34,18 @@ def segment_stage_to_prim(ui, xi, segment_length, rand_bead, num_beads):
     return xi
 
 def get_harmonic_potential(positions, mass, frequency, num_beads, left_wall):
+    # If using the entire chain
     if num_beads == len(positions):
         pot = 0
         pot = 0.5 * mass * (frequency**2) * np.dot(positions,positions)
+    # If using a segment of the chain
     else:
         pot = 0
         for k in range(1,num_beads+1):
             pot += 0.5 * mass * (frequency**2) * positions[(left_wall+k) % len(positions)]**2
     return pot
 
+# Proposal move for the whole chain
 def set_proposal_1(coords):
     # Transform entire chain to staged coordinates
     staged = get_closed_chain_stage_coords(coords, len(coords))
@@ -48,6 +54,7 @@ def set_proposal_1(coords):
     # Transform back to primitive coordinates
     return closed_chain_inverse_stage_coords(staged, len(coords))
 
+# Proposal move for a segment of the chain
 def set_proposal_2(coords, segment_length, left_wall, inv_temp, masses, omega):
     staged = []
     # Sample the guassian distribution j times.
@@ -80,7 +87,10 @@ for k in range(1,j+1):
     m_k[k-1] = (k+1) * m / k
 
 virial = []
+#============================================================================
+# PIMC starts here!!!
 for i in range(n_steps):
+    # Move the whole chain
     if i !=0 and i % (pbeads/j) == 0:
         # Save initial primitives
         old_coords = copy.deepcopy(primitives)
@@ -102,6 +112,7 @@ for i in range(n_steps):
         for o in range(pbeads):
             sumv += (m*w**2)*primitives[o]**2
         virial.append(sumv / pbeads)
+    # Or move a segment of the chain
     else:
         # Pick a random bead.
         l = np.random.randint(pbeads)
@@ -125,7 +136,8 @@ for i in range(n_steps):
         for o in range(pbeads):
             sumv += (m*w**2)*primitives[o]**2
         virial.append(sumv / pbeads)
-
+# PIMC ends here!
+#============================================================================
 print("Percentage of acceptances: ", (accept/n_steps)*100)
 # Compute cumulative average of the virial estimator
 cume = np.zeros(len(virial))
