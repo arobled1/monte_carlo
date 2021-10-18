36d9dfb80c5b8530bd8db917937c86eb74982e15
Alan Robledo <ar6138@nyu.edu>
2021-10-17 23:49:25 -0400

    created variables for z params

--- temp_accel_mc/tamc.py
+++ temp_accel_mc/tamc.py
@@ -1,34 +1,33 @@
+import numba
+from numba import jit
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import opimc_tamc_funcs as funcs
 
-
-def tamc_run(prim_z, vel_z, prims_r, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp):
+@jit(nopython=True)
+def tamc_run(prim_z, vel_z, prims_r, num_md_steps, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp, z_mass, dt, gamma, k_const, inverse_temp_bar):
     lastu_bead = np.zeros(0)
     # Computing bead masses
     masses = funcs.get_masses(num_pi_beads, num_intermediates, num_groups, real_mass)
     # Computing bead frequencies
     bead_frequencies = funcs.get_bead_frequencies(num_pi_beads, num_intermediates, num_groups, prim_omegap)
     # Call MC to generate forces
-    force_z, prims_r = funcs.run_pimc(prims_r, prim_z, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp, masses, bead_frequencies)
+    force_z, prims_r = funcs.run_pimc(prims_r, prim_z, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp, masses, bead_frequencies, k_const)
     lastu_bead = np.append(lastu_bead, prims_r[0] - prims_r[num_pi_beads])
     #=============================================================================
     # MD code starts here !! (constants below should be moved to outside block)
-    z_mass = 0.42
-    dt = 0.0003
-    gamma = 0.1
-    for i in range(1,5):
+    for i in range(num_md_steps):
         # Update velocity
         vel_z = funcs.upd_velocity(vel_z, force_z, dt, z_mass)
         # Update position
         prim_z = funcs.upd_position(prim_z, vel_z, dt)
         # Add random velocity
-        vel_z = funcs.rand_kick(vel_z, gamma, 1/inverse_temp, z_mass, dt)
+        vel_z = funcs.rand_kick(vel_z, gamma, 1/inverse_temp_bar, z_mass, dt)
         # Update position with random velocity
         prim_z = funcs.upd_position(prim_z, vel_z, dt)
         # Call MC here to update forces
-        force_z, prims_r = funcs.run_pimc(prims_r, prim_z, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp, masses, bead_frequencies)
+        force_z, prims_r = funcs.run_pimc(prims_r, prim_z, num_mc_steps, num_pi_beads, num_intermediates, num_groups, real_mass, real_freq, prim_omegap, inverse_temp, masses, bead_frequencies, k_const)
         lastu_bead = np.append(lastu_bead, prims_r[0] - prims_r[num_pi_beads])
         # Update velocity based on random force
         vel_z = funcs.upd_velocity(vel_z, force_z, dt, z_mass)
@@ -37,18 +36,25 @@ def tamc_run(prim_z, vel_z, prims_r, num_mc_steps, num_pi_beads, num_intermediat
 
 #=====================================================================
 # Block is for parameters
-n_steps = 40                             # Number of MD steps for z
+md_steps = 100                             # Number of MD steps for z
 inv_temp = 15.8/3.0                          # kT
 w = 3.0                                      # Set Frequency
 m = 0.01                                     # Set Mass
-num_beads = 32                               # Number of beads
-jay = 4                                      # Number of beads in chain segment
-big_n = 8                                    # Number of endpoint beads
+num_beads = 24                               # Number of beads
+jay = 6                                      # Number of beads in chain segment
+big_n = 4                                    # Number of endpoint beads
 freq_P = np.sqrt(num_beads) / inv_temp       # Set w_P
+# z parameters below
+inv_temp_bar = 0.001*inv_temp
+mu = 0.42
+mc_steps = 5
+deltat = 0.0001
+friction = freq_P
+k_constant = 0.3
 #======================================================================
 
 initial_xpos = np.asarray([float(x.split()[0]) for x in open('x_pos.txt').readlines()])
 initial_zpos = np.asarray([float(x.split()[0]) for x in open('z_pos.txt').readlines()])
 initial_zvel = np.asarray([float(x.split()[0]) for x in open('z_vel.txt').readlines()])
 
-last_bead = tamc_run(initial_zpos[0], initial_zvel[0], initial_xpos, n_steps, num_beads, jay, big_n, m, w, freq_P, inv_temp)
+last_bead = tamc_run(initial_zpos[0], initial_zvel[0], initial_xpos, md_steps, mc_steps, num_beads, jay, big_n, m, w, freq_P, inv_temp, mu, deltat, friction, k_constant, inv_temp_bar)
